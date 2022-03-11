80fac4d75be76ea65678d8329644b2716b3f93e4
JonPG <jon.papell@gmail.com>
2022-03-11 12:30:49 +0100

    la imatge es mou amb el teclat

---
+++ Sprite.gd
@@ -0,0 +1,22 @@
+extends Sprite
+
+
+var velocitat = Vector2(300,0)
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rotation_degrees = 90
+	position = Vector2(500,300)
+	velocitat = Vector2(100, 200)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
